56ac139e1ab2f9a7bcd8eb7bdd0830a5f4f93085
beni <b3nu_me@yahoo.com>
2016-11-15 23:01:00 +0200

    Project Create

---
+++ Ignition/Ignition.Repo/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignition.Repo.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<igniteDataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Ignition.Repo.igniteDataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ Ignition/Ignition.Repo/igniteDataContext.cs
@@ -0,0 +1,27 @@
+﻿using Ignition.Repo.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignition.Repo
+{
+    class igniteDataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public igniteDataContext() : base(GetConnectionString())
+        {
+
+        }
+
+        private static string GetConnectionString()
+        {
+            //return ConfigurationManager.ConnectionStrings["eRecruitment"].ConnectionString;
+            return @"Data Source=.\SQLEXPRESS;Initial Catalog=ignite;Integrated Security=true;";
+        }
+    }
+
+}
